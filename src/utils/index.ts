a6b187d0da57de7458e574d4e9844c7821588cdb
Artem Ryzhov <78762986+suchrile@users.noreply.github.com>
2023-06-03 01:04:11 +0300

    added new method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,7 @@ export const removeHtml = (value: string) => {
 export const getAvatarPlaceholder = (name: string) => {
   return `https://ui-avatars.com/api?name=${name}&bold=true&size=256&background=random&rounded=true`
 }
+
+export const capitalizeFirstCharacter = (value: string) => {
+  return value.charAt(0).toUpperCase() + value.slice(1)
+}
