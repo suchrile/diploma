82f2ca859c9d26bba5f438126840fd227908e287
Artem Ryzhov <78762986+suchrile@users.noreply.github.com>
2023-06-03 04:50:29 +0300

    added favourites actions

--- src/components/MoviePageHeader.vue
+++ src/components/MoviePageHeader.vue
@@ -2,17 +2,31 @@
   <div class="movie-page-header">
     <MoviePageHeaderInfo :movie="movie" class="movie-page-header__info" />
     <MoviePageRatings v-if="Object.keys(movie.rating).length" :rating="movie.rating" class="movie-page-header__ratings" />
-    <UiButtonLarge icon="bookmark-regular" class="movie-page-header__button">
-      Добавить в избранное
-    </UiButtonLarge>
+    <div class="movie-page-header__actions">
+      <UiButtonLarge icon="heart-regular" :tinted="movie.isInWishlist" class="movie-page-header__button" @click="handleWishlistButtonClick">
+        {{ movie.isInWishlist ? 'В желаемом' : 'В желаемое' }}
+      </UiButtonLarge>
+      <UiButtonLarge icon="bookmark-regular" :tinted="movie.isInFavourites" class="movie-page-header__button" @click="handleFavouritesButtonClick">
+        {{ movie.isInFavourites ? 'В избранном' : 'В избранное' }}
+      </UiButtonLarge>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
-defineProps({
+const props = defineProps({
   movie: { type: Object, required: true }
 })
+const emit = defineEmits(['add-to-wishlist', 'remove-from-wishlist', 'remove-from-favourites', 'add-to-favourites'])
+
+const handleWishlistButtonClick = () => {
+  props.movie.isInWishlist ? emit('remove-from-wishlist') : emit('add-to-wishlist')
+}
+
+const handleFavouritesButtonClick = () => {
+  props.movie.isInFavourites ? emit('remove-from-favourites') : emit('add-to-favourites')
+}
 
 </script>
 
@@ -21,7 +35,9 @@ defineProps({
   &__ratings {
     margin-top: 6px;
   }
-  &__button {
+  &__actions {
+    display: flex;
+    gap: 10px;
     margin-top: 12px;
   }
 }
