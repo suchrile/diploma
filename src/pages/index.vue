4bc995fecb201f2a4175086e372ed099c4ff495f
Artem Ryzhov <78762986+suchrile@users.noreply.github.com>
2023-06-03 12:01:23 +0300

    added empty state

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,15 @@
     <h1 class="feed-page__title">
       Лента
     </h1>
-    <div v-if="posts.length" class="feed-page__content">
+    <LoaderView v-if="isLoading" class="feed-page__loader" />
+    <div v-else-if="posts.length" class="feed-page__content">
       <InfinityScroll class="feed-page__list" @trigger="fetchData">
         <PostView v-for="(post, index) in posts" :key="post.id" v-model="posts[index]" />
       </InfinityScroll>
     </div>
-    <LoaderView v-else-if="isLoading" class="feed-page__loader" />
+    <div v-else class="feed-page__empty">
+      Подписывайтесь на других пользователей и следите за их постами
+    </div>
   </div>
 </template>
 
@@ -27,10 +30,10 @@ const fetchData = async () => {
   if (posts.value.length === total.value) { return }
   isLoading.value = true
   const { data } = await useApiFetch('/api/posts/following', { query: { page: page.value++ } })
-  if (data) {
-    total.value = data.total
-    posts.value = [...posts.value, ...data.docs]
-  }
+  // if (data) {
+  //   total.value = data.total
+  //   posts.value = [...posts.value, ...data.docs]
+  // }
   isLoading.value = false
 }
 
@@ -56,6 +59,17 @@ useHead({
   }
   &__loader {
     height: calc(100% - 80px);
+  }
+  &__empty {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: calc(100% - 80px);
+    padding: 0 15px;
+    font-size: 17px;
+    text-align: center;
+    line-height: 1.2;
+    opacity: 0.5;
   }
 }
 </style>
